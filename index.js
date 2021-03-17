0f5e7b0918ba8f764b60de6e2a464b1c94868250
StealthHydra179 <aidenm888@gmail.com>
2021-03-16 21:20:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function calculate(form) {
     let equation = form.equation.value.toString();
     let result = "";
     try {
-
+        equation = fix(equation);
+        checkEquation(equation);
         /*
         brackets
         - Check diff number of brackets
@@ -28,12 +29,79 @@ function calculate(form) {
     output.textContent = result;
 }
 
+
+
 function evaluate(equation, result = NaN) {
     if (count(equation,'(') === 0) {
         return result;
     }
     let current = count(equation, '(')
     let eval = equation.substring(last(equation, '(') + 1,first(equation,')'));
+
+    /*
+    Do operations here in order of BEDMAS
+    level 0 = multiplication & division
+    level 1 = addition + subtraction
+     */
+
+    let next = next(eval, 0);
+
+
+}
+
+
+//done
+function next(equation, level) {
+    switch (level){
+        case 0:
+            for (let i = 0; i < equation.length; i++) {
+                switch (equation.charAt(i)) {
+                    case 'x':
+                        return 'x';
+                    case '/':
+                        return '/';
+                }
+            }
+
+
+            break;
+        case 1:
+            for (let i = 0; i < equation.length; i++) {
+                switch (equation.charAt(i)) {
+                    case '+':
+                        return '+';
+                    case '-':
+                        return '-';
+                }
+            }
+
+            break;
+    }
+}
+
+function fix(equation) {
+    let final = ""
+    for (let i = 0; i < equation.length; i++) {
+        switch(equation.charAt(i)) {
+            case '*':
+                final += 'x'
+                break;
+            case'÷':
+                final += '/'
+                break;
+            default:
+                final+=equation.charAt(i)
+        }
+    }
+}
+
+function checkEquation(equation) {
+    let allowedChars = "1234567890x*/÷+-"
+    for (let i = 0; i < equation.length; i++) {
+        if (!allowedChars.includes(equation.charAt(i))) {
+            throw("Invalid Symbol (" + (equation.charAt(i)) + ")")
+        }
+    }
 }
 
 function last(equation,character) {
